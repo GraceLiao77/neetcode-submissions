28f51855648fd99246d2a3772e3dd1ddbb3699f5
Grace Liao <liaojin111@gmail.com>
2026-05-16 18:13:01 +1200

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        seen = defaultdict(int)
+        l = 0
+        r = 0
+        maxfrenquent = 0
+        maxres = 0
+        while r <= (len(s) - 1):
+            seen[s[r]] += 1
+            maxfrenquent = self.findmaxFrenquent(seen)
+            if r-l+1-maxfrenquent <= k:
+                maxres = max(r-l+1, maxres)
+                print('if maxres:', maxres, 'seen:',seen, 'l:',l, 'r:',r, 'maxfrenquent:',maxfrenquent)
+                r += 1
+            else:
+                seen[s[l]] -= 1
+                print('el maxres:', maxres, 'seen:',seen, 'l:',l, 'r:',r, 'maxfrenquent:',maxfrenquent)
+                l += 1
+                r += 1
+        return maxres
+
+    def findmaxFrenquent(self, seen: dict(int)) -> int:
+        maxres = 0
+        for val in seen.values():
+            maxres = max(maxres, val)
+        return maxres
+
+            
