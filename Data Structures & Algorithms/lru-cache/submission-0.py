89505175f66c7218a29d0f4b3b0158791b44a28b
Grace Liao <liaojin111@gmail.com>
2026-06-22 14:26:05 +0800

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.mapdict = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.mapdict:
+            self.mapdict.move_to_end(key) # 移到最后
+            return self.mapdict[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.mapdict[key] = value
+        self.mapdict.move_to_end(key)
+        if self.cap < len(self.mapdict):
+            self.mapdict.popitem(last=False)
+        print(self.mapdict)
