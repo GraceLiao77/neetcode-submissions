fc38c411b20808cf16de1332831a1ca54b62b1a5
Grace Liao <liaojin111@gmail.com>
2026-07-24 17:25:11 +0800

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        res = [0] * 26
+        for i in range(len(s)):
+            res[ord(s[i]) - ord('a')] += 1
+            res[ord(t[i]) - ord('a')] -= 1
+        
+        for j in res:
+            if j != 0:
+                return False
+        return True
+            
+
+
+        # res = defaultdict(int)
+        # for i in s:
+        #     if i not in res:
+        #         res[i] = 1
+        #     else:
+        #         res[i] += 1
+        
+        # for j in t:
+        #     if j in res:
+        #         res[j] -= 1
+        #         if res[j] == 0:
+        #             del res[j]
+        #     else:
+        #         return False
+        # if not res:
+        #     return True
+        # return False
+
